cb95edcc2e2b8a52f74f9646156392ea53c083e9
Bruno Marques <brunocm@pm.me>
2022-05-02 00:35:29 +0200

    styling

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -8,7 +8,7 @@ function Gallery(props) {
   return (
     <>
       <h1 className="font-bold font-sans break-normal text-gray-900 pb-2 text-3xl md:text-2xl">Images</h1>
-      <div className="container grid md:grid-cols-2 sm:grid-cols-1 gap-2 mx-auto">
+      <div className="container grid md:grid-cols-2 sm:grid-cols-1 gap-2 mx-auto mb-20">
         {images.map((image) => (
           <LazyLoadImage
             className="w-full md:h-64 sm:h-max object-cover rounded"
